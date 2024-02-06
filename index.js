a54313fa76c85b8dfbc467b35b89c2ebe70245f7
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2024-02-06 23:37:39 +0100

    fixed import

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { ltlToAutomaton } from './src/gpsl2automata.js';
 import { link, readBlock, readExpression } from './src/gpslReader.js';
-import { GPSLAutomatonSemantics } from './src/gpslSemantics.js';
+import { GPSLAutomatonSemantics } from './src/gpslAutomatonSemantics.js';
 
 export { ltlToAutomaton, link, readBlock, readExpression, GPSLAutomatonSemantics }
